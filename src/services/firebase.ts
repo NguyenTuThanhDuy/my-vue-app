64c115ddbc54c9a06c97a10cd78c718d40e46292
Duy Nguyen <duyntt0110@gmail.com>
2024-11-05 14:33:32 +0700

    hding firebase credentials

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getMessaging, getToken, Messaging } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCWRQFsPoPUH2UCNpjpmPzjYdWeDbE8gRk",
-  authDomain: "manganotificationapp.firebaseapp.com",
-  projectId: "manganotificationapp",
-  storageBucket: "manganotificationapp.appspot.com",
-  messagingSenderId: "952987087734",
-  appId: "1:952987087734:web:9848642b9f8d7bda330057",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
